24d5697476634c5737853065f30643b11fcec1e2
chenx97 <chenx97@gmail.com>
2016-10-13 23:59:06 -0700

    Tiny style fix
    the fix is done by eclipse's built-in function.

--- SongCrazy.java
+++ SongCrazy.java
@@ -1,25 +1,20 @@
-/*
+/* 
  * Without any limit of programming knowledge,
  * let's try to produce an awesome class for generating that song!
  */
 public class SongCrazy {
 
     public static void main(String[] args) {
-        String[] animals = {"spider", "bird", "cat", "dog", "fennekin"};
-        String[] secondline = {
-            "That wriggled and iggled and jiggled inside her.",
-            "How absurd to swallow a bird.",
-            "Imagine that to swallow a cat.",
-            "What a hog to swallow a dog.",
-            "I think she's burnin'."
-        };
+        String[] animals = { "spider", "bird", "cat", "dog", "fennekin" };
+        String[] secondline = { "That wriggled and iggled and jiggled inside her.", "How absurd to swallow a bird.",
+                "Imagine that to swallow a cat.", "What a hog to swallow a dog.", "I think she's burnin'." };
         startLine("fly", ".");
         end();
         for (int x = 0; x < 5; x++) {
-            startLine(animals[x],",");
+            startLine(animals[x], ",");
             System.out.println(secondline[x]);
             for (int y = x; y > 0; y--) {
-                swallowAtoCatchB(animals[y], animals[y-1]);
+                swallowAtoCatchB(animals[y], animals[y - 1]);
             }
             swallowAtoCatchB("spider", "fly");
             end();
